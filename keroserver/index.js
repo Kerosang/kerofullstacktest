75b87a195d4a8980d9eef6fb9c43aac1543f98f0
Kerosang <64691546+Kerosang@users.noreply.github.com>
2022-02-24 12:40:07 +0700

    aaa

--- keroserver/index.js
+++ keroserver/index.js
@@ -3,7 +3,7 @@ server = express();
 const port = 5500;
 server.get('/', function (req, res) {
     console.log('welcome :' + req.ip);
-    res.send('Kero' + req.ip + '  สวัสดีครับทุกคน');
+    res.send('Kero' + req.ip + '  สวัสดีครับทุกคนsss');
 
 });
 server.use(function (req, res, next) {
