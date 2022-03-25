7e0c77d3d1cf11eb42132c758a0a4ca8d2b34aed
Kerosang <chalawan0007@gmail.com>
2022-03-25 15:08:08 +0700

    kkk

--- keroserver/index.js
+++ keroserver/index.js
@@ -1,10 +1,22 @@
+
 const express = require('express');
+const PDFGenerator = require('pdfkit')
+const fs = require('fs')
+
+
 server = express();
 const port = 5500;
+
+
 server.get('/', function (req, res) {
     console.log('welcome :' + req.ip);
     res.send('Keroxxxxx' + req.ip + '  สวัสดีครับทุกคนsss');
 
+});
+
+server.get('/k', function (req, res) {
+    console.log('welcome :' + req.ip);
+    res.send('Keroxxxxx' + req.ip + 'KOB');
 });
 server.use(function (req, res, next) {
     next();
