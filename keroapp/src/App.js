cd66847c83043764fdaa7abadb246f894c21efcd
Kerosang <chalawan0007@gmail.com>
2022-03-30 14:28:17 +0700

    AAA

--- keroapp/src/App.js
+++ keroapp/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="AA1111noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
-    </div>
-  );
+function App() {
+  return <Button variant="contained">Hello World</Button>;
 }
 
 export default App;
--- keroserver/index.js
+++ keroserver/index.js
@@ -1,26 +1,45 @@
-
 const express = require('express');
-const PDFGenerator = require('pdfkit')
-const fs = require('fs')
-
-
-server = express();
+// const PDFGenerator = require('pdfkit')
+// const fs = require('fs')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+var server = express();
 const port = 5500;
 
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: "drttech3005",
+    database: 'drt'
+});
+
+server.use(cors());
 
 server.get('/', function (req, res) {
     console.log('welcome :' + req.ip);
     res.send('Keroxxxxx' + req.ip + '  สวัสดีครับทุกคนsss');
-
 });
-
+server.post('/register', jsonParser, function (req, res, next) {
+    connection.execute(
+        'INSERT INTO user (email,password,fname,lname) VALUES (?,?,?,?)',
+        [req.body.email, req.body.password, req.body.fname, req.body.lname],
+        function (err, results, fields) {
+            if (err) {
+                res.json({ status: "error", message: err })
+                return
+            }
+            res.json({ status: "ok" })
+        }
+    );
+});
 server.get('/k', function (req, res) {
     console.log('welcome :' + req.ip);
     res.send('Keroxxxxx' + req.ip + 'KOB');
 });
-server.use(function (req, res, next) {
-    next();
-})
+
 
 server.listen(port, () => {
     console.log(` OK: http://localhost:${port}`);
